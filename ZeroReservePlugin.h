e5112d339a5131bfa2bdaf84713d0c3ecc456b77
zeroreserve <anu@debian.localdomain>
2013-07-08 19:53:47 +0200

    added license header

--- ZeroReservePlugin.h
+++ ZeroReservePlugin.h
@@ -1,4 +1,22 @@
-#pragma once
+/*
+    This file is part of the Zero Reserve Plugin for Retroshare.
+
+    Foobar is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef ZERORESERVEPLUGIN_H
+#define ZERORESERVEPLUGIN_H
 
 #include <retroshare/rsplugin.h>
 #include <retroshare-gui/mainpage.h>
@@ -29,4 +47,6 @@ class ZeroReservePlugin: public RsPlugin
 		mutable MainPage* mainpage ;
 		mutable QIcon* mIcon ;
 };
+
+#endif
 
