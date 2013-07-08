18887fd521fff951e773bfd175d52d3d61b971ae
zeroreserve <anu@debian.localdomain>
2013-07-08 11:37:04 +0200

    Filling friends list

--- ZeroReserveDialog.cpp
+++ ZeroReserveDialog.cpp
@@ -7,4 +7,11 @@ ZeroReserveDialog::ZeroReserveDialog(QWidget *parent)
 : MainPage(parent)
 {
     ui.setupUi(this);
+
+    /* initialize friends list */
+    ui.friendSelectionWidget->setHeaderText(tr("Friend List:"));
+    ui.friendSelectionWidget->setModus(FriendSelectionWidget::MODUS_MULTI);
+    ui.friendSelectionWidget->setShowType(FriendSelectionWidget::SHOW_GROUP | FriendSelectionWidget::SHOW_SSL);
+    ui.friendSelectionWidget->start();
+
 }
