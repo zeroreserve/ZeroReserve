849ec9ca98fd08744e8e7de91524c997df1001ff
Anu <anu@zerostate.net>
2013-09-27 19:55:30 +0200

    Added files for compile without Bitcoin support (for developers only)

---
+++ ZrDummyBitcoin.h
@@ -0,0 +1,37 @@
+/*
+    This file is part of the Zero Reserve Plugin for Retroshare.
+
+    Zero Reserve is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Zero Reserve is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Zero Reserve.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+#ifndef ZRDUMMYBITCOIN_H
+#define ZRDUMMYBITCOIN_H
+
+#include "zrtypes.h"
+
+
+class ZrDummyBitcoin : public ZR::Bitcoin
+{
+public:
+    ZrDummyBitcoin();
+
+    virtual ZR::RetVal start();
+    virtual ZR::RetVal stop();
+    virtual ZR::RetVal commit();
+    virtual ZR::ZR_Number getBalance();
+};
+
+#endif // ZRDUMMYBITCOIN_H
