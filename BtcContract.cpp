daf19ef7b4ef6fb6e86776bc96cae92c6a465e3a
Anu <anu@zerostate.net>
2014-03-31 19:41:24 +0200

    Comments and class skeleton

--- BtcContract.cpp
+++ BtcContract.cpp
@@ -1,5 +1,62 @@
-#include "BtcContract.h"
+/*
+    This file is part of the Zero Reserve Plugin for Retroshare.
 
-BtcContract::BtcContract()
+    Zero Reserve is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Zero Reserve is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Zero Reserve.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include "BtcContract.h"
+#include "ZRBitcoin.h"
+
+const unsigned int BtcContract::reqConfirmations = 6;
+std::vector< BtcContract* > BtcContract::contracts;
+
+
+void BtcContract::loadContracts()
 {
+
+}
+
+void BtcContract::pollContracts()
+{
+    for( ContractIterator it = contracts.begin(); it != contracts.end(); it++ ){
+        (*it)->poll();
+    }
+}
+
+
+BtcContract::BtcContract( const std::string & txId, const ZR::ZR_Number & fiatAmount, const std::string & currencySym ):
+    m_txId( txId ),
+    m_fiatAmount( fiatAmount ),
+    m_currencySym( currencySym )
+{
+}
+
+void BtcContract::poll()
+{
+    unsigned int confirmations = ZR::Bitcoin::Instance()->getConfirmations( m_txId );
+    if( confirmations >= reqConfirmations ){
+        execute();
+    }
+}
+
+void BtcContract::persist()
+{
+
+}
+
+void BtcContract::execute()
+{
+
 }
--- BtcContract.h
+++ BtcContract.h
@@ -1,10 +1,59 @@
+/*
+    This file is part of the Zero Reserve Plugin for Retroshare.
+
+    Zero Reserve is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Zero Reserve is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Zero Reserve.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef BTCCONTRACT_H
 #define BTCCONTRACT_H
 
+#include "zrtypes.h"
+#include "Currency.h"
+
+/**
+ * @brief Bitcoin sale contract
+ *
+ * This class implements a local contract of money transfer with a friend. The money
+ * is released when an agreed upon Bitcoin transaction has at least 6 confirmations.
+ */
+
 class BtcContract
 {
+public:    
+    BtcContract( const std::string & txId, const ZR::ZR_Number & fiatAmount, const std::string & currencySym );
+
+    /** make sure we survive a crash or a shutdown once we're committed */
+    void persist();
+    /** last steps of this deal - remove from DB */
+    void finalize();
+
+private:
+    void poll();
+    void execute();
+
+private:
+    std::string m_txId;
+    ZR::ZR_Number m_fiatAmount;
+    std::string m_currencySym;
+
 public:
-    BtcContract();
+    typedef std::vector< BtcContract* >::const_iterator ContractIterator;
+    /** container for all active btcContracts */
+    static std::vector< BtcContract* > contracts;
+    static void loadContracts();
+    static void pollContracts();
+    static const unsigned int reqConfirmations;
 };
 
 #endif // BTCCONTRACT_H
